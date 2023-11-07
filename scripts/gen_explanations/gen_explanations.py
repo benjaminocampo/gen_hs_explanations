c19a2c6e6fb7c314d298817986018d67d957cc5d
nocampo <nocampo@nef-devel2.inria.fr>
2023-11-07 17:35:50 +0100

    Add missing login to huggingface_hub + remove leftover pieces of code

--- scripts/gen_explanations/gen_explanations.py
+++ scripts/gen_explanations/gen_explanations.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from transformers import T5Tokenizer, T5ForConditionalGeneration, GPTNeoXForCausalLM, GPTNeoXTokenizerFast
 from src.preprocessing import flatten_dict
 from tqdm import tqdm
+from huggingface_hub import login
 import hydra
 import pandas as pd
 import torch
@@ -24,6 +25,8 @@ def run_experiment(cfg: DictConfig, run: mlflow.ActiveRun):
         logger.info(
             f"Raw command-line arguments: {' '.join(map(shlex.quote, sys.argv))}"
         )
+        # Login to huggingface with read permissions.
+        login(token=cfg.input.read_token)
 
         # Check if a GPU is available and if not, fall back to CPU
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -35,15 +38,11 @@ def run_experiment(cfg: DictConfig, run: mlflow.ActiveRun):
         labels = test_df["label_gold"].str.upper().unique()
 
         # Initialize the T5 model and tokenizer
-        #model = T5ForConditionalGeneration.from_pretrained(cfg.input.pretrained_model_name_or_path).to(device)
-        #tokenizer = T5Tokenizer.from_pretrained(cfg.input.pretrained_model_name_or_path)
-
-        model = GPTNeoXForCausalLM.from_pretrained(cfg.input.pretrained_model_name_or_path).to(device)
-        tokenizer = GPTNeoXTokenizerFast.from_pretrained(cfg.input.pretrained_model_name_or_path)
+        model = T5ForConditionalGeneration.from_pretrained(cfg.input.pretrained_model_name_or_path).to(device)
+        tokenizer = T5Tokenizer.from_pretrained("t5-small")
 
         # Replace the placeholders in the original prompt with the list contents:
-        prompt = cfg.input.prompt.replace('LABELS = ', f'LABELS = {str(labels)}').replace('TARGETS = ', f'TARGETS = {str(all_targets)}')
-        #prompt = cfg.input.prompt
+        prompt = cfg.input.prompt.replace('TARGETS = ', f'TARGETS = {str(all_targets).lower()}')
 
         # Initialize an empty list to store explanations
         explanations = []
@@ -51,13 +50,11 @@ def run_experiment(cfg: DictConfig, run: mlflow.ActiveRun):
         # Batch size for checkpointing
         batch_size = cfg.input.checkpoint_batch_size
 
-        test_df = test_df.sample(2)
-
         # Loop through each message in the 'test_case' column
         for idx, hateful_message in tqdm(enumerate(test_df['text']), total=len(test_df)):
+
             # Combine the prompt and the hateful message
-            input_text = prompt + hateful_message
-            #input_text = f"explain: {hateful_message}"
+            input_text = prompt.replace("Given a message: ", f"Given a message: {hateful_message}")
             
             # Encode the text into tensor of integers using the appropriate tokenizer
             input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to(device)
