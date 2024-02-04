3a29897dc49e912f39465161b12ee9b991ae38d1
Benjamin Ocampo <nicolasbenjaminocampo@gmail.com>
2024-02-04 12:35:53 +0100

    Add config files. Change from environment.yml to requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name="gen_hs_explanations",
-      description="GEN HS EXPLANATIONS",
-      author="NICOLAS BENJAMIN OCAMPO",
-      author_email="nicolas-benjamin.ocampo@unice.fr",
+      description="gen_hs_explanations",
+      author="Nicolás Benjamín Ocampo",
+      author_email="nicolas-benjamin.ocampo@univ-cotedazur.fr",
       version="1.0",
       license='BSL',
       packages=find_packages())
